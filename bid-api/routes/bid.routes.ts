0e057d7b43114b8097dc7db3642a31b42ab72896
abderrahmaneMustapha <abderrahmanemustapha030898@gmail.com>
2023-02-21 12:10:25 +0100

    feature: allow normal user to access bid hisotry endpoint

--- bid-api/routes/bid.routes.ts
+++ bid-api/routes/bid.routes.ts
@@ -2,11 +2,11 @@ import express from "express";
 import controller from "../controllers/bid.controllers"
 import makeHttpCallback from "./utils/make-callback"
 import makeAuth from "./utils/make-auth"
-import { authUser, authAdmin } from "../use-cases/user"
+import { authUser } from "../use-cases/user"
 
 const router = express.Router();
 router.post('/bid', makeAuth(authUser), makeHttpCallback(controller.createBid))
-router.get('/bid', makeAuth(authAdmin), makeHttpCallback(controller.listBid))
+router.get('/bid', makeAuth(authUser), makeHttpCallback(controller.listBid))
 router.get('/bid/:name/max', makeAuth(authUser), makeHttpCallback(controller.highestBid))
 
 export default router
